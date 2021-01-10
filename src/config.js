cd773bf98624c3a1dc74f1dfb5688835d32fabf8
unknown <m.ibrahim99x@gmail.com>
2021-01-10 21:16:43 +0200

    SQL authentication mode

--- src/config.js
+++ src/config.js
@@ -1,30 +1,35 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
 var sequelize = new Sequelize({
     dialect: 'mssql',
-    // dialectModulePath: 'sequelize-msnodesqlv8',
     dialectOptions: {
-        
-        instanceName: '.',
-        validateBulkLoadParameters: true,
-        trustedConnection: true
-        
+        driver: 'SQL Server Native Client 11.0',
+        instanceName: 'MSSQLSERVER',
+        options: {
+            encrypt: false,
+            validateBulkLoadParameters: true
+        }
+        //   trustedConnection: true
     },
+    username: 'root',
+    password: '123456',
     host: 'localhost',
-    database: 'master'
+    database: 'winchdb'
 });
 
-async function testConnection(){
+async function testConnection() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-      } catch (error) {
+    } catch (error) {
         console.error('Unable to connect to the database:', error);
-      }
+    }
 }
 
 
 module.exports = {
     sequelizeSession: sequelize,
-    testConn : testConnection
+    testConn: testConnection
 }
+
+testConnection();

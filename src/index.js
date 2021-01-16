45cc5d956e0021ab7f7138fa02eee68c031b4ab5
unknown <m.ibrahim99x@gmail.com>
2021-01-16 03:49:06 +0200

    Few new packages for debugging and logging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-// My includes
 const express = require('express');
-const bodyParser = require('body-parser')
+const morgan = require('morgan');
+const logDebugger = require('debug')('app:index');
+
+// const bodyParser = require('body-parser')
 const app = express();// Created a new web server
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));// parse application/x-www-form-urlencoded
+app.use(express.json());// parse application/json
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny')); // Used for logging the requests. Will use this probably only on the development not production.
+    logDebugger('We are on DEVELOPMENT MODE.');
+    logDebugger('Morgan is enabled.'); 
+}
+
+
+
 
 
 // Load all the routes
@@ -15,5 +24,5 @@ app.use('/', require('./routes'));
 // listen on the port 
 const port = process.env.PORT || 3000;// GETS the port from the Environmet variables.
 app.listen(port, () => {
-    console.log(`listening on port => ${port}`);
+    logDebugger(`listening on port => ${port}`);
 });
